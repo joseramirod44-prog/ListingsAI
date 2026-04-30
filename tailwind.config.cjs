28bebc868bf5ef1ff7f9d76c4d880f86fbf5ed19
joseramirod44-prog <joseramirod44@gmail.com>
2026-04-29 23:05:27 -0500

    Update Tailwind CSS config to include additional content paths

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,10 +2,12 @@
 module.exports = {
   content: [
     "./index.html",
+    "./*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
